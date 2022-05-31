655ca755121cf6688730919733308f34bec6d639
baccrie <bakarerilwan1@gmail.com>
2022-05-31 13:52:04 +0100

    Task 1

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,9 +1,19 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-if ((number % 10) > 5):
-    print(f"Last digit of {number} is {number % 10} and is greater than 5")
-elif ((number % 10) == 0):
-    print(f"Last digit of {number} is {number % 10} and is 0")
-elif ((number % 10) < 6 & (number % 10) != 0):
-    print(f"Last digit of {number} is {number % 10} and is less than 6 and not 0")
+
+if (number > 0):
+    if ((number % 10) > 5):
+        print(f"Last digit of {number} is {number % 10} and is greater than 5")
+    elif ((number % 10) == 0):
+        print(f"Last digit of {number} is {number % 10} and is 0")
+    elif ((number % 10) < 6 & (number % 10) != 0):
+        print(f"Last digit of {number} is \
+                {number % 10} and is less than 6 and not 0")
+else:
+    if ((number % 10) > 5):
+         print(f"Last digit of {number} is {(-1) * (number % 10)} and is greater than 5")
+    elif ((number % 10) == 0):
+        print(f"Last digit of {number} is {(-1) * (number % 10)} and is 0")
+    elif ((number % 10) < 6 & (number % 10) != 0):
+        print(f"Last digit of {number} is {(-1) * (number % 10)} and is less than 6 and not 0")
