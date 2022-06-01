3626fe7c732a7835451d69e7011be7cdcfed3c02
baccrie <bakarerilwan1@gmail.com>
2022-06-01 15:41:27 +0100

    Task 2

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,8 @@
-alpha = 'a'
-while (alpha <= 'z'):
-    print(alpha, end = "")
-    alpha++
+#!/usr/bin/python3
+# Printing a to z using ascii codes a to z represent 97 to 122
+
+asciiAlpha = 97
+
+while asciiAlpha <= 122:
+    print(f"{asciiAlpha:c}", end='')
+    asciiAlpha += 1
