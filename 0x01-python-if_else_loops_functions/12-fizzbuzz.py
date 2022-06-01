31b01474c4d2fdff55e3e0950d2c32a7a50603eb
baccrie <bakarerilwan1@gmail.com>
2022-06-01 22:13:16 +0100

    Task 12 Fix FizzBuzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -3,7 +3,7 @@ def fizzbuzz():
     i = 1
     while (i < 101):
         if ((i % 3) == 0 and (i % 5) == 0):
-            print("Fizz Buzz ", end='')
+            print("FizzBuzz ", end='')
         elif ((i % 3) == 0):
             print("Fizz ", end='')
         elif (i % 5) == 0:
