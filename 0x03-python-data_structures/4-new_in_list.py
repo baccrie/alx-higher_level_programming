13b510f04ec5d80c4a4a7787e8b9fce3dafb2b3e
baccrie <bakarerilwan1@gmail.com>
2022-06-05 13:27:47 +0100

    Task 4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,13 +1,5 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    new = my_list
+    new = my_list[:]
     new[idx] = element
     return (new)
-
-my_list = [1, 2, 3, 4, 5]
-idx = 3
-new_element = 9
-new_list = new_in_list(my_list, idx, new_element)
-
-print(new_list)
-print(my_list)
