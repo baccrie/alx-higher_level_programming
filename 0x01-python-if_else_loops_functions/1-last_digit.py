4dc49d8826ccb98b290155fecc13acc3826f5d24
baccrie <bakarerilwan1@gmail.com>
2022-06-11 12:22:37 +0100

    Task 1

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
-import random
 import math
+import random
 number = random.randint(-10000, 10000)
-# The import math above allows me to use the fmod methos below
-# The variable rem gets the non absolute remainder of the modulus
 rem = int(math.fmod(number, 10))
 if (rem > 5):
-    print(f"Last digit of {number} is {rem} and is greater than 5")
+    print(f"Last digit of {number:d} is {rem:d} and is greater than 5")
 elif (rem == 0):
-    print(f"Last digit of {number} is {rem} and is 0")
-elif ((rem < 6) & (rem != 0)):
-    print(f"Last digit of {number} is {rem}\
- and is less than 6 and not 0")
+    print(f"Last digit of {number:d} is {rem:d} and is {rem:d}")
+else:
+    print(f"Last digit of {number:d} is {rem:d} and is less than\
+ 6 and not 0")
