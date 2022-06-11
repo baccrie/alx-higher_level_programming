6c4ef3df0a08ee2722186d0f9fc4651ca2e84d98
baccrie <bakarerilwan1@gmail.com>
2022-06-11 11:44:49 +0100

    Task 0

---
+++ 0x01-python-if_else_loops_functions/func.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+def add(a, b):
+    return (a + b)
+
+def addNumbers(a, b):
+
+    print("Sum is ", a + b)
+
+
+def subtractNumbers(a, b):
+
+    print("Difference is ", a-b)
+
+
+def multiplyNumbers(a, b):
+
+    print("Product is ", a * b)
+
+
+def divideNumbers(a, b):
+
+    print("Division is ", a / b)
+
