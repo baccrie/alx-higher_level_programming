d50ca9eb0c57a437ce9a74add3c1bf41e1fd1459
baccrie <bakarerilwan1@gmail.com>
2022-06-12 10:05:53 +0100

    Task 9

--- 0x03-python-data_structures/test.py
+++ 0x03-python-data_structures/test.py
@@ -9,4 +9,10 @@ def max_integer(my_list=[]):
         if (my_list[i] > maxNum):
             maxNum = my_list[i]
 
+
     return (maxNum)
+
+
+my_list = [1, 90, 2, 13, 34, 5, -13, 3]
+max_value = max_integer(my_list)
+print("Max: {}".format(max_value))
