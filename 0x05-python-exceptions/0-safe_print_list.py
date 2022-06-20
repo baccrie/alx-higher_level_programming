a1734b3a9d1f5a715b05267c427ad7e00f593f58
baccrie <bakarerilwan1@gmail.com>
2022-06-20 11:37:11 +0100

    Task 1 and fixed tasks 2

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,20 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    try :
+    try:
         i = 0
         while (i < x):
-            print(my_list[i], end = '')
+            print(my_list[i], end='')
             i += 1
-    except :
+    except:
         i += 0
+
     print('')
     return (i)
-
-my_list = [1, 2, 3, 4, 5]
-
-nb_print = safe_print_list(my_list, 2)
-print("nb_print: {:d}".format(nb_print))
-nb_print = safe_print_list(my_list, len(my_list))
-print("nb_print: {:d}".format(nb_print))
-nb_print = safe_print_list(my_list, len(my_list) + 2)
-print("nb_print: {:d}".format(nb_print))
---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    try:
+        print("{:d}".format(value))
+    except:
+        return (False)
+
+    return (True)
+
+
+value = 89
+has_been_print = safe_print_integer(value)
+if not has_been_print:
+    print("{} is not an integer".format(value))
+
+value = -89
+has_been_print = safe_print_integer(value)
+if not has_been_print:
+    print("{} is not an integer".format(value))
+
+value = "School"
+has_been_print = safe_print_integer(value)
+if not has_been_print:
+    print("{} is not an integer".format(value))
