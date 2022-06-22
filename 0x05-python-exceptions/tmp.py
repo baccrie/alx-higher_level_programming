746dce0a46eb572b51254b102861d76bf679a83b
baccrie <bakarerilwan1@gmail.com>
2022-06-22 10:39:38 +0100

    Task 2

--- 0x05-python-exceptions/tmp.py
+++ 0x05-python-exceptions/tmp.py
@@ -1,14 +1,11 @@
-#!/bin/python3
+#!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    i = 0
-    a = 0
-    while (i < x):
+    j = 0
+    for i in range(x):
         try:
             print("{:d}".format(my_list[i]), end='')
-            a += 1
         except (ValueError, TypeError):
-            print(end='')
-
-        i += 1
-    print('')
-    return (a)
+            continue
+        j += 1
+    print()
+    return (j)
