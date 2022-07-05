8848795d37fae9739aba7564ad770b89b8028bc8
baccrie <bakarerilwan1@gmail.com>
2022-07-05 21:04:35 +0100

    Task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,30 +5,15 @@
 
 import json
 import sys
+import os.path
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
 i = len(sys.argv)
 jsonList = []
 for a in range(1, i):
     jsonList.append(sys.argv[a])
-
-
-def save_to_json_file(my_obj, filename):
-    """
-    The function dumps the obj to the filename
-    """
-    with open(filename, 'w', encoding="UTF-8") as f:
-        json.dump(my_obj, f)a
-
-
-def load_from_json_file(filename):
-    """
-    A function that dumps from JSON to script
-    """
-    with open(filename, 'r', encoding="UTF-8") as f:
-        new = json.load(f)
-        return (new)
-
 
 save_to_json_file(jsonList, 'add_item.json')
 load_from_json_file('add_item.json')
