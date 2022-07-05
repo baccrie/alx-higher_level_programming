bab81a73fdb26f3efd67b4b9d953898d4583e6b1
baccrie <bakarerilwan1@gmail.com>
2022-07-05 18:27:35 +0100

    Task 3

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
-
 """
-Will be updated later
+Later things nigga
 """
 
-
-if __name__ == '__main__':
+if __name__ == "main":
     import json
 
     def to_json_string(my_obj):
         """
-        Will be updated later
+        Will be updated later After the saga
         """
         jsn = json.dumps(my_obj)
         return (jsn)
