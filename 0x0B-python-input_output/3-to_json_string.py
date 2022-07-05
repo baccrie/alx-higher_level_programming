50360e54a717f8b547cac4dba0f21cc25ab5e48a
baccrie <bakarerilwan1@gmail.com>
2022-07-05 18:57:08 +0100

    Task 3

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
 from json import dumps
-"""
-Later things nigga
-"""
 
 
 def to_json_string(my_obj):
-    """
-    Will be updated later After the saga
-    """
-    return (dumps(my_obj))
+    return dumps(my_obj)
