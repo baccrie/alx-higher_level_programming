c754b9f66306efb529f8a5b571b7b25352a7a980
baccrie <bakarerilwan1@gmail.com>
2022-07-05 17:39:04 +0100

    Task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,4 +7,5 @@ This script reads a text file in UTF-8 format and prints outout to screen
 def read_file(filename=""):
     """ Function that reads to screen"""
     with open(filename, 'r', encoding="UTF-8") as f:
-        print(f.read())
+        for line in f:
+            print(line, end='')
