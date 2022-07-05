e9f4e134262c5993361a7203ebb584d04dc438da
baccrie <bakarerilwan1@gmail.com>
2022-07-05 18:11:05 +0100

    Task 3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""
+Will be updated later
+"""
+
+
+if __name__ == '__main__':
+    import json
+
+    def to_json_string(my_obj):
+        """
+        Will be updated later
+        """
+        jsn = json.loads(my_obj)
+        return (jsn)
