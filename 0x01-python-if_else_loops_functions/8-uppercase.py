d25c49baed7cd9d301e4b7c90734c4fa38f49552
baccrie <bakarerilwan1@gmail.com>
2022-07-06 11:18:49 +0100

    Task 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
 def uppercase(str):
-    i = 0
     diff = ord('a') - ord('A')
-
-    while (i < len(str)):
-        con = ord(str[i])
-        if (con <= 122) and (con >= 97):
-            con -= diff
-            char = chr(con)
+    for i in range(len(str)):
+        if (97 <= ord(str[i]) <= 122):
+            a = ord(str[i]) - diff
+            b = chr(a)
         else:
-            char = str[i]
-        print("{}".format(char), end='')
-        i += 1
-    print("")
+            b = str[i]
+        print("{}".format(b), end='')
+    print('')
--- 0x01-python-if_else_loops_functions/test.py
+++ 0x01-python-if_else_loops_functions/test.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-def islower(c):
-    if 97 <= ord(c) <= 122:
-        return (True)
-    else:
-        return (False)
+def uppercase(str):
+    diff = ord('a') - ord('A')
+    for i in range(len(str)):
+        if (97 <= ord(str[i]) <= 122):
+            a = ord(str[i]) - diff
+            b = chr(a)
+        else:
+            b = str[i]
+        print("{}".format(b), end='')
+    print('')
 
 
-
-print("a is {}".format("lower" if islower("a") else "upper"))
-print("H is {}".format("lower" if islower("H") else "upper"))
-print("A is {}".format("lower" if islower("A") else "upper"))
-print("3 is {}".format("lower" if islower("3") else "upper"))
-print("g is {}".format("lower" if islower("g") else "upper"))
-
+uppercase("best")
+uppercase("Best School 98 Battery street")
