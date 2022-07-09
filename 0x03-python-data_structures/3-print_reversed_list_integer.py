f411e84ce0045f750d7904d79f6308af9fc6c21b
baccrie <bakarerilwan1@gmail.com>
2022-07-09 20:14:28 +0100

    Task 3

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     if not my_list:
-        pass
-    else:
-        my_list.reverse()
-        for i in range(len(my_list)):
-            print("{:d}".format(my_list[i]))
+        return
+
+    i = len(my_list)
+    while (i >= 0):
+        print("{:d}".format(i))
+        i -= 1
--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    newString = my_string[:]
     i = 0
 
-    for mem in my_string:
-        if (mem == 'c' or mem == 'C'):
-            del newString[i]
-        i += 1
-    return (newString)
+    newStr = 
+print(no_c("Best School"))
+print(no_c("Chicago"))
+print(no_c("C is fun!"))
