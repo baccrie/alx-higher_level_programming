a2b114ad92493c987754b44f079e3e757be99661
baccrie <bakarerilwan1@gmail.com>
2022-07-10 08:34:20 +0100

    Task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if not sentence:
-        first = None
+        first = "None"
     else:
         length = len(sentence)
         first = sentence[0]
