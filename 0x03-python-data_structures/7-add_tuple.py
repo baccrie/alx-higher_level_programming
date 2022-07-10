cda7cb3eaae0b8e8d2e8be2153ab2d904a9e49c0
baccrie <bakarerilwan1@gmail.com>
2022-07-10 08:00:12 +0100

    Task 12

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,21 +1,31 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    l1 = len(tuple_a)
-    l2 = len(tuple_b)
+    lta = len(tuple_a)
+    ltb = len(tuple_b)
 
-    a0, a1, b0, b1 = 0, 0, 0, 0
-    if (l1 == 1):
-        a0 = tuple_a[0]
-    elif (l1 >= 2):
-        a0 = tuple_a[0]
-        a1 = tuple_a[1]
-    if (l2 == 1):
-        b0 = tuple_b[0]
-    elif (l2 >= 2):
-        b0 = tuple_b[0]
-        b1 = tuple_b[1]
+    if (lta == 1):
+        ta2 = 0
+    if (lta == 0):
+        ta2 = 0
+        ta1 = 0
+    if (ltb == 1):
+        tb2 = 0
+    if (ltb == 0):
+        tb1 = 0
+        tb2 = 0
+    if (lta >= 2):
+        ta1 = tuple_a[0]
+        ta2 = tuple_a[1]
+    if (ltb >= 2):
+        tb1 = tuple_b[0]
+        tb2 = tuple_b[1]
 
-    sumTupleA = a0 + b0
-    sumTupleB = b1 + a1
-    newTuple = (sumTupleA, sumTupleB)
-    return (newTuple)
+    new = (ta1 + tb1, ta2 + tb2)
+
+tuple_a = (1, 89)
+tuple_b = (88, 11)
+new_tuple = add_tuple(tuple_a, tuple_b)
+print(new_tuple)
+
+print(add_tuple(tuple_a, (1, )))
+print(add_tuple(tuple_a, ()))
