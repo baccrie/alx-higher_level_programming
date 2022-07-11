3fb40db85066275af17577e487a96a5033aa78b8
baccrie <bakarerilwan1@gmail.com>
2022-07-11 17:36:22 +0100

    Task 8
    Done

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -3,21 +3,6 @@ def simple_delete(a_dictionary, key=""):
     if key not in a_dictionary:
         pass
     else:
-        a_dictionary.pop(key)
+        del a_dictionary[key]
 
-
-print_sorted_dictionary = \
-    __import__('6-print_sorted_dictionary').print_sorted_dictionary
-
-a_dictionary = { 'language': "C", 'Number': 89, 'track': "Low", 'ids': [1, 2, 3] }
-new_dict = simple_delete(a_dictionary, 'track')
-print_sorted_dictionary(a_dictionary)
-print("--")
-print_sorted_dictionary(new_dict)
-
-print("--")
-print("--")
-new_dict = simple_delete(a_dictionary, 'c_is_fun')
-print_sorted_dictionary(a_dictionary)
-print("--")
-print_sorted_dictionary(new_dict)
+    return (a_dictionary)
