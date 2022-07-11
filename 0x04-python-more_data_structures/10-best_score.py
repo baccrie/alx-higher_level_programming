025dc0f9f77c18f024b6960a83938a8dd00107d4
baccrie <bakarerilwan1@gmail.com>
2022-07-11 11:22:32 +0100

    Task 9 & 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
+    max = 0
     if not a_dictionary:
-        return (None)
+        return None
+    else:
+        for a in a_dictionary:
+            if a_dictionary[a] > max:
+                max = a_dictionary[a]
+            else:
+                pass
 
-    return (max(a_dictionary, key=a_dictionary.get))
+    return (max)
--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,13 +1,5 @@
 #!/usr/bin/python3
-def best_score(a_dictionary):
-    max = 0
-    if not a_dictionary:
-        return None
-    else:
-        for a in a_dictionary:
-            if a_dictionary[a] > max:
-                max = a_dictionary[a]
-            else:
-                pass
-
-    return (max)
+def multiply_by_2(a_dictionary):
+    new = {key: a_dictionary[key] for key in a_dictionary}
+    
+    return (new)
