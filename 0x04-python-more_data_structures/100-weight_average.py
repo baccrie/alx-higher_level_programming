098b35786c803989760e18323841b4dd5c17d2e9
baccrie <bakarerilwan1@gmail.com>
2022-07-11 12:29:03 +0100

    Adv task

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,6 +2,8 @@
 def weight_average(my_list=[]):
     totalScore = 0
     totalWeight = 0
+    if not my_list:
+        return (0)
     for a in my_list:
         totalScore += (a[0] * a[1])
         totalWeight += a[1]
