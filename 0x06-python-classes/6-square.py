30d837f7d8891bd2f4e3d8457f27b482cb0cd11a
baccrie <bakarerilwan1@gmail.com>
2022-07-13 23:20:33 +0100

    Task 6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -34,9 +34,11 @@ class Square:
     @position.setter
     def position(self, value):
         """a setter for attr position"""
-        if (type(value) is not tuple and (value[0] < 0 or value[1] < 0)):
+        if (type(value) is not tuple or (value[0] < 0 or value[1] < 0)):
             raise TypeError("position must be a tuple of 2 positive integers")
         elif (len(value) != 2):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif (type(value[0] is not int or value[1] is not int)):
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = value
@@ -44,14 +46,32 @@ class Square:
     def my_print(self):
         if (self.__size == 0):
             print('')
-        elif (self.__position[1] > 0) and self.__position[0] == 0:
+        elif (self.__position[0] == 0):
             for i in range(self.__size):
-                print(self.__size * '#')
-        else:
+                print(self.__size * "#", end='')
+                print()
+        elif (self.__position[0] > 0):
             for i in range(self.__size):
-                print(self.__position[0] * ' ', end='')
-                print(self.__size * '#')
-
+                print(" " * self.__position[0], end='')
+                print(self.__size * "#", end='')
+                print()
     def area(self):
         """Returns the area of square"""
         return (self.__size ** 2)
+
+
+
+my_square_1 = Square(3)
+my_square_1.my_print()
+
+print("--")
+
+my_square_2 = Square(3, (1, 1))
+my_square_2.my_print()
+
+print("--")
+
+my_square_3 = Square(3, (3, 0))
+my_square_3.my_print()
+
+print("--")
