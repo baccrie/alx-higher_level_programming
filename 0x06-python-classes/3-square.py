a8581ab10a9685a407dbbf0b00dd477ff22d955c
baccrie <bakarerilwan1@gmail.com>
2022-07-13 21:21:31 +0100

    Task 3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
-"""Create a Class Square with size and method of area"""
+"""
+A module that contains a Class
+"""
 
 
 class Square:
-    """Class - Square"""
+    """an class with size attributes"""
 
     def __init__(self, size=0):
-        """Constructor of a Square with the size"""
+        """initialises size to a private attr"""
         if (type(size) is not int):
-            raise (TypeError("size must be an integer"))
+            raise TypeError("size must be an integer")
         elif (size < 0):
-            raise (ValueError("size must be >= 0"))
+            raise ValueError("size must be >= 0")
         else:
             self.__size = size
 
     def area(self):
-        """Method to get the area of the Square"""
+        """Returns the area of Square"""
         return (self.__size ** 2)
