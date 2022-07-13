a46234d048b7942c58e98ffbb20f5d55f1c208fc
baccrie <bakarerilwan1@gmail.com>
2022-07-13 22:00:51 +0100

    Task 5

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,47 +1,36 @@
 #!/usr/bin/python3
 """
-Create a Class Square with:
-- size proprety
-- method of area and method of print_square
-- getters & setters.
+A module that contains a Class
 """
 
 
 class Square:
-    """Class - Square"""
-
+    """an class with size attributes"""
     def __init__(self, size=0):
-        """Constructor of a Square with the size"""
-        if (type(size) is not int):
-            raise (TypeError("size must be an integer"))
-        elif (size < 0):
-            raise (ValueError("size must be >= 0"))
-        else:
-            self.__size = size
+        """A constructor that initializes size"""
+        self.__size = size
 
     def area(self):
-        """Method to get the area of the Square"""
+        """Returns the area of Square"""
         return (self.__size ** 2)
-
-    def my_print(self):
-        """Method to print a Square"""
-        if (self.__size == 0):
-            print()
-        else:
-            for rows in range(self.__size):
-                print("#" * self.__size)
 
     @property
     def size(self):
-        """Getter of the private attribute size"""
+        """getter"""
         return (self.__size)
 
     @size.setter
     def size(self, value):
-        """Setter for the size private attribute"""
+        """a setter"""
         if (type(value) is not int):
-            raise (TypeError("size must be an integer"))
+            raise TypeError("size must be an integer")
         elif (value < 0):
-            raise (ValueError("size must be >= 0"))
+            raise ValueError("size must be >= 0")
         else:
             self.__size = value
+
+    def my_print(self):
+        if (self.__size == 0):
+            print('')
+        else:
+            print(self.__size * '#')
