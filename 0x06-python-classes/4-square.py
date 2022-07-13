7b2b214530307f9cc1402ef3ec2a9dcfafe8974d
baccrie <bakarerilwan1@gmail.com>
2022-07-13 21:29:46 +0100

    Task 4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,34 +1,30 @@
 #!/usr/bin/python3
-"""Create a Class Square with size, method of area and getters & setters"""
+"""
+A module that contains a Class
+"""
 
 
 class Square:
-    """Class - Square"""
-
+    """an class with size attributes"""
     def __init__(self, size=0):
-        """Constructor of a Square with the size"""
-        if (type(size) is not int):
-            raise (TypeError("size must be an integer"))
-        elif (size < 0):
-            raise (ValueError("size must be >= 0"))
-        else:
-            self.__size = size
-
-    def area(self):
-        """Method to get the area of the Square"""
-        return (self.__size ** 2)
-
-    @property
-    def size(self):
-        """Getter of the private attribute size"""
-        return (self.__size)
+        """A constructor that initializes size"""
+        self.__size = size
 
     @size.setter
     def size(self, value):
-        """Setter for the size private attribute"""
+        """a setter"""
         if (type(value) is not int):
-            raise (TypeError("size must be an integer"))
+            raise TypeError("size must be an integer")
         elif (value < 0):
-            raise (ValueError("size must be >= 0"))
+            raise ValueError("size must be >= 0")
         else:
             self.__size = value
+
+    @property
+    def size(self):
+        """getter"""
+        return (self.__size)
+
+    def area(self):
+        """Returns the area of Square"""
+        return (self.__size ** 2)
