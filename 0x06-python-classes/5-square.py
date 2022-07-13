c1d54a95a0b1bb4538d1a83bd1b9821da9294377
baccrie <bakarerilwan1@gmail.com>
2022-07-13 22:03:43 +0100

    Task 5

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -33,4 +33,5 @@ class Square:
         if (self.__size == 0):
             print('')
         else:
-            print(self.__size * '#')
+            for i in range(self.__size):
+                print(self.__size * '#')
