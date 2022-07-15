08080a8e24357830b3104bbda0eb10414ef44af7
baccrie <bakarerilwan1@gmail.com>
2022-07-15 22:18:27 +0100

    Task 8 Fixed

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -64,7 +64,7 @@ class Rectangle:
 
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
-        if (type(rect_1 is not Rectangle)):
+        if (type(rect_1) is not Rectangle):
             raise TypeError("rect_1 must be an instance of Rectangle")
         elif (type(rect_2) is not Rectangle):
             raise TypeError("rect_2 must be an instance     of Rectangle")
