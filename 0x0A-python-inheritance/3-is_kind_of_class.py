6699827a1a9cb3315985b11796c352e4552c25fa
baccrie <bakarerilwan1@gmail.com>
2022-07-19 19:06:33 +0100

    Task 3

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -4,15 +4,7 @@
 
 def is_kind_of_class(obj, a_class):
 
-    if (isinstance(obj, a_class) or isinstance(obj, super(a_class, obj))):
+    if (isinstance(obj, a_class)):
         return (True)
     else:
         return (False)
-
-a = 1
-if is_kind_of_class(a, int):
-    print("{} comes from {}".format(a, int.__name__))
-if is_kind_of_class(a, float):
-    print("{} comes from {}".format(a, float.__name__))
-if is_kind_of_class(a, object):
-    print("{} comes from {}".format(a, object.__name__))
