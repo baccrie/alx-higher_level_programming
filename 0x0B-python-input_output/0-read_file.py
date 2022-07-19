40b0501391445440589340b9afbfb3dce808acd0
baccrie <bakarerilwan1@gmail.com>
2022-07-19 22:31:46 +0100

    Task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,5 +6,5 @@
 
 def read_file(filename=""):
     """Function opens a file for.reading"""
-    with open(filename, encoding="UTF-8") as f:
+    with open(filename, 'r',  encoding="utf-8") as f:
         print(f.read())
