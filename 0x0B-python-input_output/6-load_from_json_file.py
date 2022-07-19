16fd56fce9a099a9a7a35b4f898ecaa68f8a4faa
baccrie <bakarerilwan1@gmail.com>
2022-07-19 23:18:28 +0100

    Task 6

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
-"""
-A JSON SCRIPT WRITTEN BY baccrie
-"""
+"""A magical module"""
+
 
 import json
 
 
 def load_from_json_file(filename):
-    """
-    A function that dumps from JSON to script
-    """
-    with open(filename, 'r', encoding="UTF-8") as f:
-        new = json.load(f)
-        return (new)
+    """JSON"""
+    with open(filename, 'w', encoding='utf-8') as f:
+        data = json.load(f)
+        return (data)
