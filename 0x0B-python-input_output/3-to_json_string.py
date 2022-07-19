b1c6b210dd8ba115952802fcf499a9b4aa220852
baccrie <bakarerilwan1@gmail.com>
2022-07-19 22:57:01 +0100

    Task 3

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,15 +1,10 @@
 #!/usr/bin/python3
-"""
-A JSON script
-"""
+"""The module performs magic"""
 
 
 import json
 
 
 def to_json_string(my_obj):
-    """
-    The function returns the json format of an object
-    """
     new = json.dumps(my_obj)
     return (new)
