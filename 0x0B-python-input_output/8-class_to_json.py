4c43d3ae1f3f395feafe43255af796f0b587ab35
baccrie <bakarerilwan1@gmail.com>
2022-07-20 22:16:30 +0100

    Task 8

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -2,11 +2,8 @@
 """A magical module"""
 
 
-import json
-
-
 def class_to_json(obj):
     """JSON serialization of class instance"""
     new = obj.__dict__
-    jsonData = json.dumps(new)
-    return (jsonData)
+    #jsonData = json.dumps(new)
+    return (new)
