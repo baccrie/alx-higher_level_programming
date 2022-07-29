b102d2590d4915daba59d65bfdc5e1b39d7f9204
baccrie <bakarerilwan1@gmail.com>
2022-07-29 21:22:38 +0100

    Task 1

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
-"""This class will be the “base” of all other classes in this project. The goal of it is to manage id attribute in all your future classes and to avoid duplicating the same code (by extension, same bugs)
+"""This class will be the “base” of all other
+classes in this project. The goal of it is to manage
+id attribute in all your future classes and to
+avoid duplicating the same code
+(by extension, same bugs)
 """
 
 
 class Base:
     """Base class"""
     __nb_objects = 0
+
     def __init__(self, id=None):
         if (id):
             self.id = id
