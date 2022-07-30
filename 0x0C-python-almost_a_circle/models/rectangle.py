f9544719893c84806f92e20e84c1aeeec1e03267
baccrie <bakarerilwan1@gmail.com>
2022-07-30 18:50:50 +0100

    Task 2

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -7,18 +7,12 @@ from .base import Base
 
 class Rectangle(Base):
     """
-    Class Rectangle this class inheritance from Base class.
+    A rectangle class
     """
     # Constructor
     def __init__(self, width, height, x=0, y=0, id=None):
         """
-        Constructor of a Rectangle
-        Args:
-          - width: int
-          - height: int
-          - x: int
-          - y: int
-          - id: int
+        Instantiation of attr
         """
         super().__init__(id)
         self.width = width
@@ -29,56 +23,42 @@ class Rectangle(Base):
     # Getters and Setters
     @property
     def width(self):
-        """Getter of the private attribute width"""
+        """width getter"""
         return (self.__width)
 
     @width.setter
     def width(self, width):
         """
-        Setter of the private attribute width
-        Args:
-          - value: int
+        width setter
         """
         self.__width = width
 
     @property
     def height(self):
-        """Getter of the private attribute height"""
+        """height getter"""
         return (self.__height)
 
     @height.setter
     def height(self, height):
-        """
-        Setter of the private attribute height
-        Args:
-          - value: int
-        """
+        """height setter"""
         self.__height = height
 
     @property
     def x(self):
-        """Getter of the private attribute x"""
+        """x getter"""
         return self.__x
 
     @x.setter
     def x(self, x):
-        """
-        Setter of the private attribute x
-        Args:
-          - value: int
-        """
+        """x setter"""
         self.__x = x
 
     @property
     def y(self):
-        """Getter of the private attribute y"""
+        """y getter"""
         return (self.__y)
 
     @y.setter
     def y(self, y):
-        """
-        Setter of the private attribute y
-        Args:
-          - value: int
-        """
+        """y setter"""
         self.__y = y
