6f31875af5374004a116c3a2af9e99c14572542d
baccrie <bakarerilwan1@gmail.com>
2022-07-31 09:53:22 +0100

    Task 10

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+A magical module that inherits from Rectangle
+"""
+
+from .rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """A class that inherits from rectangle"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        width = size
+        height = size
+        Rectangle.__init__(self, width, height, x=x, y=y, id=None)
+
+    def __str__(self):
+        return (f"[square] ({self.id}) {self.x}/{self.y} - {self.width}")
