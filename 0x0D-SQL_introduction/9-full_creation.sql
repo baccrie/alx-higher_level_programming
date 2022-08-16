3e8b6d86a7a94d2635fa8a82799f3917e64c1032
baccrie <bakarerilwan1@gmail.com>
2022-08-16 20:30:21 +0100

    Task 9

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,6 +1,6 @@
 -- Creates a new table with parameters
 CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
-INSERT INTO second_table VALUES (1, "John", 10);
-INSERT INTO second_table VALUES (2, "Alex", 3);
-INSERT INTO second_table VALUES (3, "bob", 14);
-INSERT INTO second_table VALUES (4, "George", 8);
+INSERT INTO second_table (id, name, score) VALUES (1, "John", 10);
+INSERT INTO second_table (id, name, score) VALUES (2, "Alex", 3);
+INSERT INTO second_table (id, name, score) VALUES (3, "bob", 14);
+INSERT INTO second_table (id, name, score) VALUES (4, "George", 8);
