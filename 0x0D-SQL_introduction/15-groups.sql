eab98c6854434113f14decf4b1531325bcd3eb5f
baccrie <bakarerilwan1@gmail.com>
2022-08-16 21:36:50 +0100

    Task 15

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,5 @@
 -- selects with caution
-SELECT score, COUNT(sc) AS number WHERE sc = score ORDER BY score;
+SELECT score, COUNT(*) AS number
+FROM second_table
+GROUP BY score
+ORDER  BY SCORE DESC;
