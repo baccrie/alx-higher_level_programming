b2d7ed997bc8827d820633e233ee6c4fe4f78343
baccrie <bakarerilwan1@gmail.com>
2022-09-06 21:24:48 +0100

    Task 2

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+if (process.argv.length === 0) {
+    console.log('No argument');
+} else if (process.argv.length === 1) {
+    console.log('Arguement found');
+} else {
+    console.log('Arguements found');
+}
