f2b22b846c11663055b233a2d13f41228dd3116f
baccrie <bakarerilwan1@gmail.com>
2022-09-06 22:29:14 +0100

    Task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -5,5 +5,5 @@ if (process.argv.length < 3) {
 } else if (process.argv.length === 3) {
     console.log('Argument found');
 } else {
-    console.log('Arguements found');
+    console.log('Arguments found');
 }
