0952d6187554e65838a481e05e8873ee7ce7d864
baccrie <bakarerilwan1@gmail.com>
2022-09-06 21:13:28 +0100

    task 1

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const first = 'C is fun';
 const second = 'Python is cool';
-const third = 'Javascript is amazing';
+const third = 'JavaScript is amazing';
 
 console.log(first);
 console.log(second);
