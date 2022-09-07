52d448b509c054a68846770625ff27e27367f5f9
baccrie <bakarerilwan1@gmail.com>
2022-09-07 22:14:48 +0100

    Task 10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,12 +1,15 @@
 #!/usr/bin/node
 
 const num = parseInt(process.argv[2]);
-let fact = 1;
 if (isNaN(num)) {
   console.log('1');
+} else {
+  function fact (n) {
+    if (n === 1) {
+      return (n);
+    } else {
+      return (n * fact(n - 1));
+    }
+  }
+  console.log(fact(num));
 }
-while (num !== 1) {
-  fact *= num;
-  num -= 1;
-}
-console.log(fact);
