1988c437c5b5b52dff76f925fb337330f17258b8
baccrie <bakarerilwan1@gmail.com>
2022-09-07 19:13:05 +0100

    Task 6

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,3 @@
+#!/usr/bin/node
+let num = process.argv[2] * 1
+console.log(num);
---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const arr = ['C is fun', 'Python is cool', 'javascript is amazing'];
+let i = 0;
+while (i < 3) {
+  console.log(arr[i]);
+  i++;
+}
