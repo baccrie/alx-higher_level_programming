5c6f1075aff4c5a22e9f6cdb64b7990339ac4830
baccrie <bakarerilwan1@gmail.com>
2022-09-07 19:34:33 +0100

    Task 7 Correction

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -5,8 +5,8 @@ let num = parseInt(arg);
 let i = 0;
 
 if (typeof num != 'number') {
-    console.log("Missing number of occurrences");
-    return;
+  console.log("Missing number of occurrences");
+  return;
 }
 while (i < num) {
  console.log("C is fun");
