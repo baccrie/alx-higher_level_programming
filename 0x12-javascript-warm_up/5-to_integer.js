a26e584acda798b975273d8dbdcb986f8cedcb4c
baccrie <bakarerilwan1@gmail.com>
2022-09-07 20:05:34 +0100

    Task 5 fixed

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,3 +1,7 @@
 #!/usr/bin/node
-let num = process.argv[2] * 1
-console.log(num);
+let num = parseInt(process.argv[2]);
+if (isNaN(num)) {
+  console.log('Not a number');
+} else {
+  console.log(`My number: ${num}`);
+}
