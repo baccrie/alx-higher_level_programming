5a42a6cd7fbe7ecae67cb95f5203c3d2a92537b4
baccrie <bakarerilwan1@gmail.com>
2022-09-10 20:29:53 +0100

    Task 8

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,9 +1,11 @@
 #!/usr/bin/node
+exports.esrever = function (list) {
+  const rev = [];
+  let size = list.length - 1;
+  for (let i = 0; i < list.length; i++) {
+    rev[i] = list[size];
+    size--;
+  }
 
-const esrever = (list) => {
-  const reversedArray = [];
-  list.forEach((element) => reversedArray.unshift(element));
-  return (reversedArray);
+  return (rev);
 };
-
-module.exports = { esrever };
