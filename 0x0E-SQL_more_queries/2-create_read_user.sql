c07bf79d3692b06b4275da006d0c83dee5a14cb8
baccrie <bakarerilwan1@gmail.com>
2022-09-16 20:41:39 +0000

    task 2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,5 @@
 #!/usr/bin/sql
 -- creates a new user and new databases with limited privilege
 CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
-CREATE DATABASE hbtn_0d_2 IF NOT EXISTS;
+CREATE DATABASE IF NOT EXISTS  hbtn_0d_2;
 GRANT SELECT ON hbtn_0d_2 . * TO user_0d_2@localhost;
