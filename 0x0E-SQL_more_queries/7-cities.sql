486e8fbf76f6c303fc64e398bdf7d466c1f64ceb
baccrie <bakarerilwan1@gmail.com>
2022-09-16 21:36:24 +0000

    Task 7

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,11 +1,5 @@
--- Create database and table
+#!/usr/bin/sql
+-- creates a new table with Null constraints
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
-id INT NOT NULL AUTO_INCREMENT,
-state_id INT NOT NULL,
-name VARCHAR(256) NOT NULL,
-PRIMARY KEY(id),
-FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states (id)
-);
-
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities (id INT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY, state NOT NULL FOREIGN KEY(id) REFRENCES state(id), name VARCHAR(256)) type=InnoDB;
