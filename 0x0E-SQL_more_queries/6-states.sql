be64d3c8c7a5180e3da078763f8d429f37425a4b
baccrie <bakarerilwan1@gmail.com>
2022-09-16 21:23:18 +0000

    task 6

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,4 +1,5 @@
 #!/usr/bin/sql
 -- creates a new table with Null constraints
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states (id INT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY , name VARCHAR(256));
