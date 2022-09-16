a5f8532d2789b4f1aeb4fc4b9a97d64e302408a4
baccrie <bakarerilwan1@gmail.com>
2022-09-16 20:07:15 +0000

    Task 0

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
 #!/usr/bin/sql
 -- this script lists user privledges
-SHOW GRANTS FOR user_0d_1@localhost, user_0d_2@localhost;
+SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR user_0d_2@localhost;
