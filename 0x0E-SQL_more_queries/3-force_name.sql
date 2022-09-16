8c04ee06ad0806644ff455687fccacbe79be82f3
baccrie <bakarerilwan1@gmail.com>
2022-09-16 20:49:17 +0000

    Task 3

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,3 +1,3 @@
--- Create table force_name
-CREATE TABLE IF NOT EXISTS force_name(id INT, name VARCHAR(256) NOT NULL);
-
+#!/usr/bin/sql
+-- creates a new table with Null constraints
+CREATE TABLE IF NOT EXISTS force_name(id INT, name VARCHAR(256) NOT NULL );
