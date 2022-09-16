95a89720f666b430fb18dfd53de054f0055c0c2a
baccrie <bakarerilwan1@gmail.com>
2022-09-16 21:06:17 +0000

    task 5 corr

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,3 +1,3 @@
 #!/usr/bin/sql
 -- creates a new table with Null constraints
-CREATE TABLE IF NOT EXISTS unique_id(id INT DEFAULT 1 UNIQUE, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS unique_id (id INT DEFAULT 1 UNIQUE, name VARCHAR(256));
