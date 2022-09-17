4624847d7e7911a9da0e0f5062fb14d0b0ba48ea
baccrie <bakarerilwan1@gmail.com>
2022-09-17 05:43:22 +0100

    T

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-/* let result;
+let result;
 if (process.argv.length === 2) {
   console.log('0');
 } else if (process.argv.length === 3) {
@@ -8,19 +8,30 @@ if (process.argv.length === 2) {
   let total = process.argv.length + 2;
   let first = parseInt(process.argv[2]);
   let i = 2;
+  let arr;
+  let j = 0;
 
   while (i < total) {
     if (parseInt(process.argv[i]) > first) {
       first = parseInt(process.argv[i]);
     }
+    arr[j] = process.argv[i];
+    j++;
     i++;
 }
+j = 0;
+i = 0;
+while (i < arr.length) {
+  if (arr[i] == first) {
+    
 console.log(first);
-} */
+}
+/**
 if (process.argv.length < 4) {
   console.log('0');
 } else {
   const data = process.argv.slice(2);
   data.sort((a, b) => b - a);
   console.log(data[1]);
-}
+  }
+  **/
