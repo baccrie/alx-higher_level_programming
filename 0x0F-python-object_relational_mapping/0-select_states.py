436a095336b06af63e8542f3d0b5c35152cbeda9
baccrie <bakarerilwan1@gmail.com>
2022-10-02 09:40:08 +0100

    fixed

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -18,7 +18,7 @@ if __name__ == '__main__':
                 db=database_name
                 )"""
 
-        db = mysql.connect(host='localhost', port=3306, user=argv[1],
+        db = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
                            passwd=argv[2], db=argv[3])
     except Exception:
         exit(0)
