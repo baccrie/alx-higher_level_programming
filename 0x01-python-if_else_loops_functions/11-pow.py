d6b6caae447de9834981feb142a8da383ac90076
baccrie <bakarerilwan1@gmail.com>
2022-10-23 14:47:06 +0100

    Task 11

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,12 +1,3 @@
 #!/usr/bin/python3
 def pow(a, b):
-    i = 0
-    if b < 0:
-        power = a ** b
-        return (power)
-    power = 1
-
-    while (i < b):
-        power *= a
-        i += 1
-    return (power)
+    return (a ** b)
