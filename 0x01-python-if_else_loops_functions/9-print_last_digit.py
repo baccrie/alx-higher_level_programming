d8d7b4e9d248c7747be2bcb3fd7bad4cd5593922
baccrie <bakarerilwan1@gmail.com>
2022-10-23 14:40:23 +0100

    Task 9

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    last = int(number / 10)
-    last = number - (last * 10)
-    if number < 0:
-        last1 = last * (-1)
-        print(f"{last1}", end='')
-        return last1
+    if (number < 0):
+        rem = (number * (-1))
+        rem = rem - ((rem // 10) * 10)
     else:
-        print(f"{last}", end='')
-        return last
+        rem = number % 10
+
+    print(rem, end="")
+    return (rem)
