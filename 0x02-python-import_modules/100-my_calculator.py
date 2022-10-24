a53d85c016ce091801b79a61fce6af5b1355308b
baccrie <bakarerilwan1@gmail.com>
2022-10-24 19:21:32 +0100

    Task 0

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,10 +2,8 @@
 from calculator_1 import add, div, sub, mul
 from sys import argv
 if __name__ == '__main__':
-    a = int(argv[1])
-    b = int(argv[3])
+    operators = ["+", "-", "*", "/"]
     length = len(argv)
-    operators = ['+', '-', '*', '/']
 
     if length != 4:
         print(f"Usage: {argv[0]} <a> <operator> <b> ")
@@ -15,13 +13,18 @@ if __name__ == '__main__':
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
     else:
+        a = int(argv[1])
+        b = int(argv[3])
+        length = len(argv)
+        result = 0
+
         if (argv[2] == '+'):
             result = add(a, b)
         elif (argv[2] == '-'):
             result = sub(a, b)
         elif (argv[2] == '*'):
             result = mul(a, b)
-        elif (argv[2] == '-'):
+        elif (argv[2] == '/'):
             result = div(a, b)
 
-    print("{} {} {} = {}".format(argv[1], argv[2], argv[3], result))
+        print("{} {} {} = {}".format(argv[1], argv[2], argv[3], result))
