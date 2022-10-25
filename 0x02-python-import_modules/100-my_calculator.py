bcd0b3ea8758c9a3ad37deed12c19bdb13078764
baccrie <bakarerilwan1@gmail.com>
2022-10-25 11:20:34 +0100

    Task adv

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -13,18 +13,16 @@ if __name__ == '__main__':
     elif (argv[2]) not in operators:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
-    else:
-        a = int(argv[1])
-        b = int(argv[3])
-        length = len(argv)
+    a = int(argv[1])
+    b = int(argv[3])
 
-        if (argv[2] == '+'):
-            result = add(a, b)
-        elif (argv[2] == '-'):
-            result = sub(a, b)
-        elif (argv[2] == '*'):
-            result = mul(a, b)
-        elif (argv[2] == '/'):
-            result = div(a, b)
+    if (argv[2] == '+'):
+        result = add(a, b)
+    elif (argv[2] == '-'):
+        result = sub(a, b)
+    elif (argv[2] == '*'):
+        result = mul(a, b)
+    elif (argv[2] == '/'):
+        result = div(a, b)
 
     print("{:d} {} {:d} = {:d}".format(a, argv[2], b, result))
