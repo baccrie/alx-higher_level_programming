8e51daf9da6bdbad23c8802b3648869b0a28564d
baccrie <bakarerilwan1@gmail.com>
2022-10-25 11:47:11 +0100

    Task 100

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,28 +1,31 @@
 #!/usr/bin/python3
-from calculator_1 import add, div, sub, mul
-from sys import argv
-if __name__ == '__main__':
-    operators = ["+", "-", "*", "/"]
-    result = 0
-    length = len(argv)
 
-    if length != 4:
-        print(f"Usage: {argv[0]} <a> <operator> <b> ")
+from sys import argv
+from calculator_1 import add, sub, mul, div
+
+if (__name__ == "__main__"):
+    argc = len(argv)
+    result = 0
+    operators = ["+", "-", "*", "/"]
+
+    if (argc != 4):
+        print(f"Usage: {argv[0]} <a> <operator> <b>")
         exit(1)
 
-    if (argv[2]) not in operators:
+    if (argv[2] not in operators):
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
-    a = int(argv[1])
-    b = int(argv[3])
+
+    num1 = int(argv[1])
     operator = argv[2]
+    num2 = int(argv[3])
 
     if (operator == "+"):
-        result = add(a, b)
+        result = add(num1, num2)
     elif (operator == "-"):
-        result = sub(a, b)
+        result = sub(num1, num2)
     elif (operator == "*"):
-        result = mul(a, b)
-    elif (operator == "/"):
-        result = div(a, b)
-    print("{:d} {:s} {:d} = {:d}".format(a, operator, b, result))
+        result = mul(num1, num2)
+    else:
+        result = div(num1, num2)
+    print("{:d} {:s} {:d} = {:d}".format(num1, operator, num2, result))
