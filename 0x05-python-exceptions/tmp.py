77d103b924d62c861cc3c33eb81a62262252071c
baccrie <bakarerilwan1@gmail.com>
2022-11-06 08:47:43 +0100

    Task 0

--- 0x05-python-exceptions/tmp.py
+++ 0x05-python-exceptions/tmp.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-def safe_print_list_integers(my_list=[], x=0):
-    j = 0
-    for i in range(x):
-        try:
-            print("{:d}".format(my_list[i]), end='')
-        except (ValueError, TypeError):
-            continue
-        j += 1
-    print()
-    return (j)
+safe_print_list = __import__('0-safe_print_list').safe_print_list
+
+my_list = [1, 2, 3, 4, 5]
+
+nb_print = safe_print_list(my_list, 2)
+print("nb_print: {:d}".format(nb_print))
+nb_print = safe_print_list(my_list, len(my_list))
+print("nb_print: {:d}".format(nb_print))
+nb_print = safe_print_list(my_list, len(my_list) + 2)
+print("nb_print: {:d}".format(nb_print))
