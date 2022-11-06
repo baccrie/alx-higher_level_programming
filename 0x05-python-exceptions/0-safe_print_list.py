38e5a4c90119dd0d0a21e60e6b23453f5d55244f
baccrie <bakarerilwan1@gmail.com>
2022-11-06 09:03:46 +0100

    Task 1

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,9 +4,8 @@ def safe_print_list(my_list=[], x=0):
     try:
         for i in range(x):
             print(my_list[i], end='')
-            num += 1
     except (Exception):
-        pass
+        num += 1
 
     print('')
     return (num)
