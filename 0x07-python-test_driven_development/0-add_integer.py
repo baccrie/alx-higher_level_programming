7f5af7cca6f6b6c3f627b958e623538df708a964
baccrie <bakarerilwan1@gmail.com>
2022-11-16 22:19:37 +0100

    Task 0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,13 +1,20 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+
+
 """
 A module that performs magic , baccrie 2022 ©
 """
 
-def add_integer(a, b=98):
 
-    if type(a) is not int or type(a) is not float:
+def add_integer(a, b=98):
+    """ The function returns the addition of a and b
+    :a - a parameter either of type int or float
+    :b - a parameter eitger of type int or float
+    """
+
+    if (type(a) is not int) and (type(a) is not float):
         raise TypeError("a must be an integer")
-    if type(b) is not int or type(b) is not float:
+    if (type(b) is not int) and (type(b) is not float):
         raise TypeError("b must be an integer")
     a = int(a)
     b = int(b)
