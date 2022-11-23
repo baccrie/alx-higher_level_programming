3a9fb7180b2558b1e5f73bd8a04e2fed78628d58
baccrie <bakarerilwan1@gmail.com>
2022-11-23 22:32:53 +0100

    Task 6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -76,9 +76,9 @@ class Square:
         a setter method for the attr position
         """
 
-        if type(value) is not tuple or \
-                len(value) != 2 or value[0] < 0 or value[1] < 0 or \
-                type(value[0]) is not int or \
-                type(value[1]) is not int:
+        if (len(value) != 2) or (type(value) is not tuple) \
+                or (type(value[0]) is not int) \
+                or (type(value[1]) is not int) \
+                or (value[0] < 0) or (value[1] < 0):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
