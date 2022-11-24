b742bb4255e614ed0061b1e6089e1f8c472a8894
baccrie <bakarerilwan1@gmail.com>
2022-11-24 20:02:52 +0100

    Task

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -56,11 +56,12 @@ class Square:
 
         if self.__size == 0:
             print()
-        for k in range(self.__position[1]):
-            print()
-        for i in range(self.__size):
-            print(self.__position[0] * ' ', end='')
-            print(self.__size * character)
+        else:
+            for k in range(self.__position[1]):
+                print()
+            for i in range(self.__size):
+                print(self.__position[0] * ' ', end='')
+                print(self.__size * character)
 
     @property
     def position(self):
