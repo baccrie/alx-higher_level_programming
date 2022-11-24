b7079c3db0a53dbad8562d8c8417785636115262
baccrie <bakarerilwan1@gmail.com>
2022-11-24 20:13:26 +0100

    Task 6.

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -20,8 +20,8 @@ class Square:
         elif size < 0:
             raise ValueError("size must be >= 0")
 
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
     def area(self):
         """
