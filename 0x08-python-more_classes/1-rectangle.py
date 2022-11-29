76bcf73d47a704d4601065d8173b9c2a57ac5c14
baccrie <bakarerilwan1@gmail.com>
2022-11-29 18:01:27 +0100

    Task 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -5,32 +5,43 @@ A module that contains a class Rectangle
 
 
 class Rectangle:
-    """An empty class"""
+    """
+    A class that defines a rectangle
+    """
     def __init__(self, width=0, height=0):
-        """Constructor"""
-        self.width = width
-        self.height = height
+        self.__width = width
+        self.__height = height
 
     @property
     def width(self):
+        """
+        getter method for width attribute
+        """
         return (self.__width)
 
     @width.setter
     def width(self, value):
-        if(type(value) is not int):
+        """
+        setter method for attr width
+        """
+        if type(value) is not int:
             raise TypeError("width must be an integer")
-        elif (value < 0):
+        elif value < 0:
             raise ValueError("width must be >= 0")
-        self.__width = value
 
     @property
     def height(self):
+        """
+        height atrr getter
+        """
         return (self.__height)
 
     @height.setter
-    def height(self, value):
-        if(type(value) is not int):
+    def width(self, value):
+        """
+        height attr setter
+        """
+        if type(value) is not int:
             raise TypeError("height must be an integer")
-        elif (value < 0):
+        elif value < 0:
             raise ValueError("height must be >= 0")
-        self.__height = value
