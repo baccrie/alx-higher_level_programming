67481ed29814e6398cd8522ca66ab328a9604d1a
baccrie <bakarerilwan1@gmail.com>
2022-12-06 21:24:16 +0100

    Task 8

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
-"""Later things"""
 
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """Tmp"""
-
+    """
+    Rectangle ingerits from BaseGeo
+    """
     def __init__(self, width, height):
-        """Tmp"""
+        """
+        instantaneous
+        """
         self.__width = width
         self.__height = height
-        BaseGeometry.integer_validator(self, 'width', width)
-        BaseGeometry.integer_validator(self, 'height', height)
+        BaseGeometry.integer_validator(self, "width", width)
+        BaseGeometry.integer_validator(self, "height", height)
