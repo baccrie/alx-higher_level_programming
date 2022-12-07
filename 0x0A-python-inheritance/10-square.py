61de80f0714a6ddaf4005efc0a1ed243409c51b3
baccrie <bakarerilwan1@gmail.com>
2022-12-07 16:57:19 +0100

    Task 10

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
-"""Later things"""
 
+"""
+A magical module
+"""
 
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """Trabaye"""
+    """
+    A class that inherits from Rectangle class
+    """
 
     def __init__(self, size):
+        """
+        instantateous
+        """
         Rectangle.__init__(self, size, size)
-        self.__size = size
-        Rectangle.integer_validator(self, 'size', size)
-
-    def area(self):
-        return (self.__size ** 2)
+        Rectangle.integer_validator(self, "size", size)
