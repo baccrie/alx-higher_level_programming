23183863038485b07c947c34bbaba3bf68d68a96
baccrie <bakarerilwan1@gmail.com>
2022-12-22 12:32:28 +0100

    Task 2

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -3,6 +3,7 @@
 
 """A module that does wonder"""
 
+
 def append_write(filename="", text=""):
     """Function that appends a string to a text file"""
     with open(filename, 'a', encoding='UTF-8') as f:
