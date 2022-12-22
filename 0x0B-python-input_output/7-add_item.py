d9f0b345185be9a63194f6048101939308d844e3
baccrie <bakarerilwan1@gmail.com>
2022-12-22 21:03:09 +0100

    Task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,26 +1,24 @@
 #!/usr/bin/python3
-"""A magical module"""
+"""Copyright 2022 © baccrie"""
 
-
-import sys
 import json
-import os.path
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+import os
+import sys
+
+
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
-if os.path.exists("add_item.json"):
-    pass
+list_to_save = []
+for i in sys.argv:
+    list_to_save.append(i)
+if len(list_to_save) >= 1:
+    list_to_save.pop(0)
+
+if os._exists('add_item.json'):
+    data = load_from_json_file('add_item.json')
+    data += list_to_save
+
 else:
-    with open("add_item.json", 'w') as f:
-        f.write("[]")
-
-n = len(sys.argv)
-
-obj = load_from_json_file("add_item.json")
-
-for i in range(1, n):
-    obj.append(sys.argv[i])
-
-
-save_to_json_file(obj, "add_item.json")
+    save_to_json_file(list_to_save, 'add_item.json')
