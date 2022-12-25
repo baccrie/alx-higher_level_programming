75719f96f81a18855904f01d8ad1243a45891b9e
baccrie <bakarerilwan1@gmail.com>
2022-12-25 11:24:04 +0100

    Task 14 & 15

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -13,3 +13,20 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """list to dict"""
+        if not list_dictionaries or list_dictionaries = "[]":
+            return ("[]")
+        return (json.dumps(list_dictionaries))
+
+    @staticmethod
+    def save_to_file(cls, list_objs):
+        """Save list objs dict to a file"""
+        if list_objs is None:
+            dict_repr = []
+        else:
+            for i in list_objs:
+                dict_repr.append(i.to_dict())
+
