c409d5ce34d571285055ba3ee7952711353061ab
baccrie <bakarerilwan1@gmail.com>
2022-12-31 20:40:01 +0100

    Task 6

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
-"""
-Contains the class definition of a State
-"""
+"""A sequel alchemy module that performs magic"""
 
-from sqlalchemy import Column, Integer, String
+
+from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
+from base import Base
+from sqlalchemy import Column, String, Integer
 
-Base = declarative_base()
+
+engine = create_engine('mysql+mysqldb://@localhost:3306', pool_pre_ping=True)
+Base.metadata.create_all(engine)
+
+session = Session(engine)
 
 
 class State(Base):
-    """
-    Representation of the table states
-    """
-    __tablename__ = 'states'
-
-    id = Column(Integer, primary_key=True)
+    """A module that is been mapped to db with alchemy"""
+    __table_name__ = 'states'
+    id = Column(integer, primary_key=True, nullable=False, unique=True, autoincrement=True)
     name = Column(String(128), nullable=False)
