d7a841af6917ac1af7fc828cf839367ab98b336b
baccrie <bakarerilwan1@gmail.com>
2022-12-31 20:54:34 +0100

    Task 6 fix

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -4,18 +4,14 @@
 
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from base import Base
 from sqlalchemy import Column, String, Integer
 
 
-engine = create_engine('mysql+mysqldb://@localhost:3306', pool_pre_ping=True)
-Base.metadata.create_all(engine)
-
-session = Session(engine)
-
+Base = declarative_base()
 
 class State(Base):
     """A module that is been mapped to db with alchemy"""
-    __table_name__ = 'states'
-    id = Column(integer, primary_key=True, nullable=False, unique=True, autoincrement=True)
+
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False, unique=True, autoincrement=True)
     name = Column(String(128), nullable=False)
