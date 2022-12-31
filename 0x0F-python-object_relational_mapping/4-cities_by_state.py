c337437d7d82a189fab655331da31d1d494d89db
baccrie <bakarerilwan1@gmail.com>
2022-12-31 19:20:14 +0100

    Task 4

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,32 +1,21 @@
 #!/usr/bin/python3
-"""Lists all cities from the database hbtn_0e_4_usa"""
+"""A module that connects to a db and lists all cities"""
+import MySQLdb
+from sys import argv
 
 if __name__ == '__main__':
-    from sys import argv
-    import MySQLdb as mysql
+    usern = argv[1]
+    passw = argv[2]
+    db = argv[3]
+    hs = 'localhost'
+    pt = 3306
 
-    if (len(argv) != 4):
-        print('Use: username, password, database name')
-        exit(1)
+    db = MySQLdb.connect(host=hs, user=usern, passwd=passw, db=db, port=pt)
+    conn = db.cursor()
+    conn.execute("""SELECT * FROM cities ORDER BY id ASC""")
+    result = conn.fetchall()
+    for x in result:
+        print(x)
 
-    try:
-        db = mysql.connect(host='localhost', port=3306, user=argv[1],
-                           passwd=argv[2], db=argv[3])
-    except Exception:
-        print('Failed to connect to the database')
-        exit(0)
-
-    cursor = db.cursor()
-
-    cursor.execute("""SELECT c.id, c.name, s.name FROM cities as c
-                      INNER JOIN states as s
-                      ON c.state_id = s.id
-                      ORDER BY c.id ASC;""")
-
-    result_query = cursor.fetchall()
-
-    for row in result_query:
-        print(row)
-
-    cursor.close()
+    conn.close()
     db.close()
