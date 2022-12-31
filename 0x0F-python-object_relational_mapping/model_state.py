e35f7751cbd15f796df5204a113273948124b014
baccrie <bakarerilwan1@gmail.com>
2022-12-31 20:59:35 +0100

    Task 6 fixxxxxxxxxxxx

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -9,9 +9,11 @@ from sqlalchemy import Column, String, Integer
 
 Base = declarative_base()
 
+
 class State(Base):
     """A module that is been mapped to db with alchemy"""
 
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, nullable=False, unique=True, autoincrement=True)
+    id = Column(Integer, primary_key=True,
+                nullable=False, unique=True, autoincrement=True)
     name = Column(String(128), nullable=False)
