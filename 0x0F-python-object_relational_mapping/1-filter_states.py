ddb3c223f3a4e2f552a0f58fc5d6afd18f3a2248
baccrie <bakarerilwan1@gmail.com>
2022-12-31 17:17:49 +0100

    Task 1 Fix

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,29 +1,19 @@
 #!/usr/bin/python3
-"""
-Lists all states with a name starting with
-N (upper N) from the database hbtn_0e_0_usa
-"""
+"""A module that connects to a db and lists all state
+starting with letter N"""
+import MySQLdb
+from sys import argv
 
 if __name__ == '__main__':
-    from sys import argv
-    import MySQLdb as mysql
+    usern = argv[1]
+    passw = argv[2]
+    db = argv[3]
+    hs = 'localhost'
+    pt = 3306
 
-    try:
-        db = mysql.connect(host='localhost', port=3306, user=argv[1],
-                           passwd=argv[2], db=argv[3])
-    except Exception:
-        print('Failed to connect to the database')
-        exit(0)
-
-    cursor = db.cursor()
-
-    cursor.execute("SELECT * FROM states \
-                    WHERE name LIKE BINARY 'N%' ORDER BY id ASC;")
-
-    result_query = cursor.fetchall()
-
-    for row in result_query:
-        print(row)
-
-    cursor.close()
-    db.close()
+    db = MySQLdb.connect(host=hs, user=usern, passwd=passw, db=db, port=pt)
+    conn = db.cursor()
+    conn.execute("""SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC""")
+    result = conn.fetchall()
+    for x in result:
+        print(x)
