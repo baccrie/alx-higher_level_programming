fcb4f5b2d9e4722466c41076ea03d1535126b637
baccrie <bakarerilwan1@gmail.com>
2023-01-01 10:52:34 +0100

    Task 11

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,32 +1,26 @@
 #!/usr/bin/python3
-"""
-Adds the State object “Louisiana” to the database hbtn_0e_6_usa
-"""
+"""A module that connects to database and reads its
+contents to stdout using Sql Alchemy
+(This alchemy gave me nightmares before
+getting to understand), but victory at last.
+Vamoos!!!"""
 
 
-if __name__ == "__main__":
-    from sqlalchemy.orm import sessionmaker
-    from sqlalchemy import create_engine
-    from model_state import Base, State
-    from sys import argv
+from sqlalchemy import create_engine
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sys import argv
 
-    if (len(argv) != 4):
-        print('Use: username, password database_name')
-        exit(1)
+if __name__ == '__main__':
+    user = argv[1]
+    passwd = argv[2]
+    db = argv[3]
 
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(user, passwd, db), pool_pre_ping=True)
     Base.metadata.create_all(engine)
-
     Session = sessionmaker(bind=engine)
     session = Session()
-
-    new_row = State(name='Louisiana')
-    session.add(new_row)
-    session.commit()
-
-    states = session.query(State).where(State.name == 'Louisiana')
-
-    for row in states:
-        print(row.id)
-    session.close()
+    new_state = State("Louisiana")
+    new_state.commit()
+    print(new_state.id)
