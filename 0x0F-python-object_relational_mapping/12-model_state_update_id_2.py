89a6be65426d7e34d60977a02911c1d1ce569774
baccrie <bakarerilwan1@gmail.com>
2023-01-01 11:38:55 +0100

    Task 12

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,27 +1,28 @@
 #!/usr/bin/python3
-"""
-Changes the name of a State object from the database hbtn_0e_6_usa
-"""
+"""A module that connects to database and
+save an instance attr to the database,
+it saves the state Louisiana to the database
+(This alchemy gave me nightmares before
+getting to understand), but victory at last.
+Vamoos!!!"""
 
 
-if __name__ == "__main__":
-    from sqlalchemy.orm import sessionmaker
-    from sqlalchemy import create_engine
-    from model_state import Base, State
-    from sys import argv
+from sqlalchemy import create_engine
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sys import argv
 
-    if (len(argv) != 4):
-        print('Use: username, password database_name')
-        exit(1)
+if __name__ == '__main__':
+    user = argv[1]
+    passwd = argv[2]
+    db = argv[3]
 
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(user, passwd, db), pool_pre_ping=True)
     Base.metadata.create_all(engine)
-
     Session = sessionmaker(bind=engine)
     session = Session()
-
-    states = session.query(State).where(State.id == 2)\
-        .update({'name': 'New Mexico'})
+    new_update = session.query(State).filter(State.id==2).first()
+    new_update.name = 'New Mexico'
+    session.add(new_update)
     session.commit()
-    session.close()
