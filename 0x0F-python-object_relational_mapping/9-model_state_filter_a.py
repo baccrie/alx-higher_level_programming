a114a74e161311c167e207faa12f36c73c0a4f75
baccrie <bakarerilwan1@gmail.com>
2023-01-01 10:06:41 +0100

    Task 9 sucks

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -21,7 +21,7 @@ if __name__ == '__main__':
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    result = session.query(State).filter(State.name.like('a')).order_by(State.id).all()
+    result = session.query(State).order_by(State.id).all()
 
     for row in result:
         if 'a' in row.name:
