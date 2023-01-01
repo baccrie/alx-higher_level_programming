2d70737caf3fd3eee350d7353e4443f335b3300a
baccrie <bakarerilwan1@gmail.com>
2023-01-01 10:02:34 +0100

    Task 8 sucks

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,29 +1,31 @@
 #!/usr/bin/python3
-"""
-Lists all State objects that contain the letter a
-from the database hbtn_0e_6_usa
-"""
+"""A module that connects to database and reads its
+contents to stdout using Sql Alchemy
+(This alchemy gave me nightmares before
+getting to understand), but victory at last.
+Vamoos!!!"""
 
-if __name__ == "__main__":
-    from sqlalchemy.orm import sessionmaker
-    from sqlalchemy import create_engine
-    from model_state import Base, State
-    from sys import argv
 
-    if (len(argv) != 4):
-        print('Use: username, password database_name')
-        exit(1)
+from sqlalchemy import create_engine
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sys import argv
 
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+if __name__ == '__main__':
+    user = argv[1]
+    passwd = argv[2]
+    db = argv[3]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(user, passwd, db), pool_pre_ping=True)
     Base.metadata.create_all(engine)
-
     Session = sessionmaker(bind=engine)
     session = Session()
+    result = session.query(State)
+    .filter(State.name.like('a')).order_by(State.id).all()
 
-    states = session.query(State).filter(State.name.like('%a%')).order_by(
-        State.id)
-
-    for row in states:
-        print(f'{row.id}: {row.name}')
-    session.close()
+    for row in result:
+        if 'a' in row.name:
+            print("{}: {}".format(row.id, row.name))
+        else:
+            pass
