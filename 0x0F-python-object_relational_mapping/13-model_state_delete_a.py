d79fbe940a1c4e008d5f46d15f3727fada987035
baccrie <bakarerilwan1@gmail.com>
2023-01-01 11:52:09 +0100

    Task 13

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,27 +1,25 @@
 #!/usr/bin/python3
-"""
-Deletes all State objects with a name containing the letter a
-from the database hbtn_0e_6_usa
-"""
+"""A module that connects to database and
+save an instance attr to the database,
+after updating its value when id = 2
+(This alchemy gave me nightmares before
+getting to understand), but victory at last.
+Vamoos!!!"""
 
 
-if __name__ == "__main__":
-    from sqlalchemy.orm import sessionmaker
-    from sqlalchemy import create_engine
-    from model_state import Base, State
-    from sys import argv
+from sqlalchemy import create_engine
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sys import argv
 
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+if __name__ == '__main__':
+    user = argv[1]
+    passwd = argv[2]
+    db = argv[3]
 
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(user, passwd, db), pool_pre_ping=True)
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-
-    states = session.query(State).filter(State.name.like('%a%'))
-
-    for state in states:
-        session.delete(state)
-
-    session.commit()
-    session.close()
+    session.query(State).filter(State.name.like('%a%')).delete()
