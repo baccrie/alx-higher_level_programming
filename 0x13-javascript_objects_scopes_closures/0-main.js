652754c202db521a6c06e1909d3ed3abc3820665
baccrie <bakarerilwan1@gmail.com>
2023-01-07 20:24:16 +0100

    Task 1

--- 0x13-javascript_objects_scopes_closures/0-main.js
+++ 0x13-javascript_objects_scopes_closures/0-main.js
@@ -1,5 +1,17 @@
 #!/usr/bin/node
-const esrever = require('./8-esrever').esrever;
+const Rectangle = require('./1-rectangle');
 
-console.log(esrever([1, 2, 3, 4, 5]));
-console.log(esrever(["School", 89, { id: 12 }, "String"]));
+const r1 = new Rectangle(2, 3);
+console.log(r1);
+console.log(r1.width);
+console.log(r1.height);
+
+const r2 = new Rectangle(2, -3);
+console.log(r2);
+console.log(r2.width);
+console.log(r2.height);
+
+const r3 = new Rectangle(2);
+console.log(r3);
+console.log(r3.width);
+console.log(r3.height);
