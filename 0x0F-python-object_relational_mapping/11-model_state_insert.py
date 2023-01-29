e023d47c7d9e304bbbf85dc4108d0403b4e7334c
baccrie <bakarerilwan1@gmail.com>
2023-01-29 21:23:04 +0100

    Task 11

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,28 +1,29 @@
 #!/usr/bin/python3
-"""A module that connects to database and
-save an instance attr to the database,
-it saves the state Louisiana to the database
-(This alchemy gave me nightmares before
-getting to understand), but victory at last.
-Vamoos!!!"""
+"""
+A magical model
+"""
 
 
-from sqlalchemy import create_engine
+from sqlalchemy import Column, String, create_engine, MetaData
+from sqlalchemy.ext.declarative import declarative_base
 from model_state import Base, State
 from sqlalchemy.orm import sessionmaker
 from sys import argv
 
+
 if __name__ == '__main__':
     user = argv[1]
-    passwd = argv[2]
+    passw = argv[2]
     db = argv[3]
 
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(user, passwd, db), pool_pre_ping=True)
+                           .format(user, passw, db), pool_pre_ping=True)
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    new_state = State(name="Louisiana")
-    session.add(new_state)
+
+    new_user = State(name='Louisiana')
+    session.add(new_user)
     session.commit()
-    print(new_state.id)
+
+    print(new_user.id)
