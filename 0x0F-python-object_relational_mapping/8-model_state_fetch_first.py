6828f4e303430897aed9e37ab3b0f2ea20de121f
baccrie <bakarerilwan1@gmail.com>
2023-01-29 20:43:43 +0100

    Task 8

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,29 +1,29 @@
 #!/usr/bin/python3
-"""A module that connects to database and reads only the first contents
-of a table to stdout using Sql Alchemy
-(This alchemy gave me nightmares before
-getting to understand), but victory at last.
-Vamoos!!!"""
+"""
+A magical model
+"""
 
 
-from sqlalchemy import create_engine
+from sqlalchemy import Column, String, create_engine, MetaData
+from sqlalchemy.ext.declarative import declarative_base
 from model_state import Base, State
 from sqlalchemy.orm import sessionmaker
 from sys import argv
 
+
 if __name__ == '__main__':
     user = argv[1]
-    passwd = argv[2]
+    passw = argv[2]
     db = argv[3]
 
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(user, passwd, db), pool_pre_ping=True)
+                           .format(user, passw, db), pool_pre_ping=True)
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    result = session.query(State).order_by(State.id).first()
 
-    if result is None:
-        print("Nothing")
+    res = session.query(State).order_by(State.id).first()
+    if not res:
+        print()
     else:
-        print("{}: {}".format(result.id, result.name))
+        print('{}: {}'.format(res.id, res.name))
