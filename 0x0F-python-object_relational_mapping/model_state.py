820fa9174506368518cc0dacc22df163255a9ea2
baccrie <bakarerilwan1@gmail.com>
2023-01-29 17:29:20 +0100

    Task 6

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,18 +1,28 @@
 #!/usr/bin/python3
-"""A sequel alchemy module that performs magic"""
 
+"""
+The Magical SqlAlchemy has started
+"""
 
+from sqlalchemy import Column, String, create_engine, MetaData
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, String, Integer
 
 
+user = 'root'
+passw = 'root'
+db = 'hbtn_0e_6_usa'
+host = 'localhost'
+
+engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
+                       .format(user, passw, host, db), pool_pre_ping=True)
 Base = declarative_base()
 
 
 class State(Base):
-    """A module that is been mapped to db with alchemy"""
-
+    """A state class that inherits from the alchemy base"""
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True,
-                nullable=False, unique=True, autoincrement=True)
-    name = Column(String(128), nullable=False)
+    id = Column(nullable=false, primary_key=True, unique=True)
+    name = Column(String(128), null=false)
+
+
+Base.metadata.create_all(engine)
--- 0x0F-python-object_relational_mapping/tmp.py
+++ 0x0F-python-object_relational_mapping/tmp.py
@@ -1,6 +1,18 @@
 #!/usr/bin/python3
+"""A sequel alchemy module that performs magic"""
 
 
-from sys import argv
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, Integer
 
-conn = MySQLdb.connect(host=hst, user=username, passwd=password,
+
+Base = declarative_base()
+
+
+class State(Base):
+    """A module that is been mapped to db with alchemy"""
+
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True,
+                nullable=False, unique=True, autoincrement=True)
+    name = Column(String(128), nullable=False)
