e04e27022fbea417cc19b11e09f5a6b78034ddaf
baccrie <bakarerilwan1@gmail.com>
2023-01-29 21:39:45 +0100

    Task 12

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,28 +1,27 @@
 #!/usr/bin/python3
-"""A module that connects to database and
-save an instance attr to the database,
-after updating its value when id = 2
-(This alchemy gave me nightmares before
-getting to understand), but victory at last.
-Vamoos!!!"""
+"""
+A magical model
+"""
 
 
-from sqlalchemy import create_engine
+from sqlalchemy import Column, String, create_engine, MetaData
+from sqlalchemy.ext.declarative import declarative_base
 from model_state import Base, State
 from sqlalchemy.orm import sessionmaker
 from sys import argv
 
+
 if __name__ == '__main__':
     user = argv[1]
-    passwd = argv[2]
+    passw = argv[2]
     db = argv[3]
 
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(user, passwd, db), pool_pre_ping=True)
+                           .format(user, passw, db), pool_pre_ping=True)
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    new_update = session.query(State).filter(State.id == 2).first()
-    new_update.name = 'New Mexico'
-    session.add(new_update)
+
+    res = session.query(State).id(2)
+    res.name = 'New Mexico'
     session.commit()
