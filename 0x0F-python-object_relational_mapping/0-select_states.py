3f358aaadd241211a53865ea68d140ed4dc4e6d8
baccrie <bakarerilwan1@gmail.com>
2023-01-29 09:56:17 +0100

    Task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,9 +8,9 @@ from sys import argv
 import MySQLdb
 
 
-username = argv[0]
-password = argv[1]
-database = argv[2]
+username = argv[1]
+password = argv[2]
+database = argv[3]
 
 if __name__ == '__main__':
     conn = MySQLdb.connect(host='localhost', user=username, passwd=password, db=database, port=3306)
