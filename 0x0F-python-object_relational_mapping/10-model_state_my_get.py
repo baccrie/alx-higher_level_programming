8d431bdb07025ec14a28e8188a58d17384942bbf
baccrie <bakarerilwan1@gmail.com>
2023-01-29 21:01:28 +0100

    Task 10

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,35 +1,31 @@
 #!/usr/bin/python3
-"""A module that connects to database and
-searches for a state and prints the result to stdout
-(This alchemy gave me nightmares before
-getting to understand), but victory at last.
-Vamoos!!!"""
+"""
+A magical model
+"""
 
 
-from sqlalchemy import create_engine
+from sqlalchemy import Column, String, create_engine, MetaData
+from sqlalchemy.ext.declarative import declarative_base
 from model_state import Base, State
 from sqlalchemy.orm import sessionmaker
 from sys import argv
 
+
 if __name__ == '__main__':
     user = argv[1]
-    passwd = argv[2]
+    passw = argv[2]
     db = argv[3]
-    search = argv[4]
-    count = 0
+    search_name = argv[4]
 
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(user, passwd, db), pool_pre_ping=True)
+                           .format(user, passw, db), pool_pre_ping=True)
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    result = session.query(State).order_by(State.id).all()
 
-    for row in result:
-        if search == row.name:
-            print("{}".format(row.id))
-            count += 1
-        else:
-            pass
-    if count == 0:
-        print("Not found")
+    res = session.query(State).order_by(State.id).all()
+    result = "Not Found"
+    for obj in res:
+        if obj.name = search_name:
+            result = obj.id
+    print(result)
