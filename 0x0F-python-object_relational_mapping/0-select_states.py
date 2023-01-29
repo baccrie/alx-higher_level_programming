05f1c9fd4a50ea455209ae37fbbfdbee5c783802
baccrie <bakarerilwan1@gmail.com>
2023-01-29 09:49:25 +0100

    Task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,21 +1,20 @@
 #!/usr/bin/python3
-"""A module that connects to a db and lists all states"""
-import MySQLdb
+
+"""
+A magical Module
+"""
+
 from sys import argv
+import MySQLdb
+
+
+username = argv[0]
+password = argv[1]
+database = argv[2]
 
 if __name__ == '__main__':
-    usern = argv[1]
-    passw = argv[2]
-    db = argv[3]
-    hs = 'localhost'
-    pt = 3306
-
-    db = MySQLdb.connect(host=hs, user=usern, passwd=passw, db=db, port=pt)
-    conn = db.cursor()
-    conn.execute("""SELECT * FROM states ORDER BY id ASC""")
-    result = conn.fetchall()
-    for x in result:
-        print(x)
-
+    conn = MySQLdb.connect(host='localhost', user=username, passwd=password, db=database, port=3306)
+    cur = conn.cursor()
+    cur.execute("""SELECT * FROM states ORDER BY id ASC""")
+    cur.close()
     conn.close()
-    db.close()
