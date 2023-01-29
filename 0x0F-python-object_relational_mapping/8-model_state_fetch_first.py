2ca6a4978e8ebd96925d640288734955b08ecab0
baccrie <bakarerilwan1@gmail.com>
2023-01-29 20:45:16 +0100

    Task 8 fixxx

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -24,6 +24,6 @@ if __name__ == '__main__':
 
     res = session.query(State).order_by(State.id).first()
     if not res:
-        print()
+        print("Nothing")
     else:
         print('{}: {}'.format(res.id, res.name))
