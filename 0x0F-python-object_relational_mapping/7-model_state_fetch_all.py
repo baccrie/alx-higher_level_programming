8e74f5fe12eca26e4ad19866ca826dc7cfa40761
baccrie <bakarerilwan1@gmail.com>
2023-01-29 19:51:48 +0100

    Task 7

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,27 +1,26 @@
 #!/usr/bin/python3
-"""A module that connects to database and reads its
-contents to stdout using Sql Alchemy
-(This alchemy gave me nightmares before
-getting to understand), but victory at last.
-Vamoos!!!"""
+"""
+A magical model
+"""
 
 
-from sqlalchemy import create_engine
+from sqlalchemy import Column, String, create_engine, MetaData
+from sqlalchemy.ext.declarative import declarative_base
 from model_state import Base, State
 from sqlalchemy.orm import sessionmaker
-from sys import argv
+
 
 if __name__ == '__main__':
-    user = argv[1]
-    passwd = argv[2]
-    db = argv[3]
+    user = 'root'
+    passw = 'root'
+    db = 'hbtn_0e_6_usa'
+    host = 'localhost'
 
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(user, passwd, db), pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
+                           .format(user, passw, host, db), pool_pre_ping=True)
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    result = session.query(State).order_by(State.id).all()
-
-    for row in result:
-        print("{}: {}".format(row.id, row.name))
+    res = session.query(State).order_by(State.id).all()
+    for obj in res:
+        print("{}: {}".format(obj.id, obj.name))
--- 0x0F-python-object_relational_mapping/tmp.py
+++ 0x0F-python-object_relational_mapping/tmp.py
@@ -26,3 +26,30 @@ class State(Base):
 
 
 Base.metadata.create_all(engine)
+#!/usr/bin/python3
+"""A module that connects to database and reads its
+contents to stdout using Sql Alchemy
+(This alchemy gave me nightmares before
+getting to understand), but victory at last.
+Vamoos!!!"""
+
+
+from sqlalchemy import create_engine
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+
+if __name__ == '__main__':
+    user = argv[1]
+    passwd = argv[2]
+    db = argv[3]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(user, passwd, db), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    result = session.query(State).order_by(State.id).all()
+
+    for row in result:
+        print("{}: {}".format(row.id, row.name))
