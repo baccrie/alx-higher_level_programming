be3b2e8485126135ab2eb12da34e0d173dab2951
baccrie <bakarerilwan1@gmail.com>
2023-01-29 17:41:57 +0100

    Fixed

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -4,25 +4,15 @@
 The Magical SqlAlchemy has started
 """
 
-from sqlalchemy import Column, String, create_engine, MetaData
+from sqlalchemy import Column, String, Integer
 from sqlalchemy.ext.declarative import declarative_base
 
 
-user = 'root'
-passw = 'root'
-db = 'hbtn_0e_6_usa'
-host = 'localhost'
-
-engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
-                       .format(user, passw, host, db), pool_pre_ping=True)
 Base = declarative_base()
 
 
 class State(Base):
     """A state class that inherits from the alchemy base"""
     __tablename__ = 'states'
-    id = Column(nullable=false, primary_key=True, unique=True)
-    name = Column(String(128), null=false)
-
-
-Base.metadata.create_all(engine)
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
