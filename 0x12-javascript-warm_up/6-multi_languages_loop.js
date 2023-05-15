9aa9ad39e3b59e25ec20560091076b5507864fb0
baccrie <bakarerilwan1@gmail.com>
2023-05-15 21:51:05 +0100

    Task 6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const myVar = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const myVar = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (const i of myVar) {
   console.log(i);
 }
