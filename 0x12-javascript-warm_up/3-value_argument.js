59b235416ea65dffa59418cf873c0334655f17a9
baccrie <bakarerilwan1@gmail.com>
2023-05-15 18:07:40 +0100

    Task 3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -4,5 +4,7 @@ if (process.argv.length == 2) {
     console.log("No argument");
 }
 else {
-    console.log(process.argv[3])
+    console.log(process.argv[2])
 }
+console.log(process.argv)
+console.log(process.argv.length)
