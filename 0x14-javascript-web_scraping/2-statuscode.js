8a15bc50f3fc6b4c1e9c06bcc0712eb5b3479e70
baccrie <bakarerilwan1@gmail.com>
2023-05-21 14:42:58 +0100

    Task 2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -4,5 +4,5 @@ const request = require('request');
 const url = process.argv[2];
 
 request(url, (err, response, body) => {
-  console.log('code: ', response.statusCode);
+  console.log('code:', response.statusCode);
 });
