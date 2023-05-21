9f02bd432d992af9a0bec8d834c32678a87f415d
baccrie <bakarerilwan1@gmail.com>
2023-05-21 15:09:19 +0100

    Task 7

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,11 @@
 #!/usr/bin/node
+
 const request = require('request');
-const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-  console.log(error || JSON.parse(body).title);
-});
+const epis = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`;
+request(epis, (err, response, body) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(JSON.parse(body).title);
+}
+);
