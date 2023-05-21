b9ebd0816670f978a316e75a47dcabb899dc3834
baccrie <bakarerilwan1@gmail.com>
2023-05-21 14:31:00 +0100

    Task 1

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,3 +1,11 @@
 #!/usr/bin/node
+
 const fs = require('fs');
-fs.writeFileSync(process.argv[2], process.argv[3]);
+const filepath = process.argv[2];
+const string_to_write = process.argv[3];
+
+fs.writeFile(filepath, string_to_write, 'utf-8', (err) => {
+  if (err) {
+    console.log(err);
+  }
+});
