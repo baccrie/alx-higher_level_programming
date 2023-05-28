5f2e6c7afa9d3e5347ed472214f8ac284cfa5f83
baccrie <bakarerilwan1@gmail.com>
2023-05-28 10:01:57 +0100

    Task 11 semistanstand fix

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -17,14 +17,14 @@ let secondBiggest = 0;
 i = 2;
 
 while (i < len) {
-  if (parseInt(num[i]) >= secondBiggest & parseInt(num[i]) != biggest) {
+  if (parseInt(num[i]) >= secondBiggest & parseInt(num[i]) !== biggest) {
     secondBiggest = parseInt(num[i]);
   }
   // console.log(biggest);
   i++;
 }
 
-if (num.length == 2) {
+if (num.length === 2) {
   secondBiggest = 0;
 }
 console.log(secondBiggest);
